9b53387e31d30f206cec28814d440e5dd2da9895
Justė Naujokaitytė <justenau@gmail.com>
2020-11-05 23:04:32 +0200

    Add missing comments

--- Reed-Muller/Coding/Decoder.cs
+++ Reed-Muller/Coding/Decoder.cs
@@ -8,16 +8,28 @@ namespace Reed_Muller.Coding
 {
     public static class Decoder
     {
-
+        /// <summary>
+        /// Decodes vector using Fast Hadamard Transform.
+        /// 0s in the vector are changed to '-1', then the encoded vector is multiplied with H matrixes and the results
+        /// and summed up together. The index of the highest absolute value in the sum vector is used for decoding.
+        /// Index value is transformed to reversed binary number with '1' in front of it the highest value 
+        /// was positive and '0' if it was negative. The final vector is the decoded vector.
+        /// </summary>
+        /// <param name="vector">Encoded vector</param>
+        /// <param name="m">Code parameter m</param>
+        /// <returns>Decoded vector</returns>
         public static Vector DecodeSingleVector (Vector vector, int m)
         {
             var transformedVector = vector.TranformBitsFromZeroToMinusOne();
 
+            // Multiply vector with H matrixes and sum up the results to a single vector
             for(int i=1; i<=m; i++)
             {
                 var calculationMatrix = HadamardTransformMatrix.GetTransformedMatrix(i, m);
                 transformedVector = MatrixUtils.MutltiplyVectorWithMatrix(transformedVector, calculationMatrix, Convert.ToInt32(Math.Pow(2, m)));
             }
+
+            // Get the index of the highest absolute value in the vector and convert it to binary representation
             var vectorDataAbsValues = transformedVector.Data.Select(x => Math.Abs(x)).ToList();
             var maxPosition = vectorDataAbsValues.IndexOf(vectorDataAbsValues.Max());
             var reversedBinaryValue = Convert.ToString(maxPosition, 2);
@@ -25,15 +37,25 @@ namespace Reed_Muller.Coding
             var zerosToAddCount = m - reversedBinaryValue.Length;
             if (zerosToAddCount != 0)
             {
-                reversedBinaryValue = new String('0', zerosToAddCount) + reversedBinaryValue;
+                reversedBinaryValue = new string('0', zerosToAddCount) + reversedBinaryValue;
             }
+
+            // Add the first digit, based on if the highest value was positive or negative
             reversedBinaryValue+=transformedVector.Data[maxPosition] < 0 ? '0' : '1';
+
             var binaryValue = reversedBinaryValue.ToCharArray();
             Array.Reverse(binaryValue);
             var decodedVectorData = ConversionUtils.ConvertStringToIntegerArray(new string(binaryValue));
             return new Vector(decodedVectorData);
         }
 
+        /// <summary>
+        /// Decoded binary sequence using Fast Hadamar Transform. All of the vectors in the received list
+        /// are decoded independently and then combined to a single decoded result vector.
+        /// </summary>
+        /// <param name="vectors">List of vectors to be decoded</param>
+        /// <param name="m">Code parameter m</param>
+        /// <returns>Decoded vector</returns>
         public static Vector DecodeBinarySequence (List<Vector> vectors, int m)
         {
             var result = new List<Vector>();
--- Reed-Muller/Coding/Encoder.cs
+++ Reed-Muller/Coding/Encoder.cs
@@ -7,6 +7,13 @@ namespace Reed_Muller.Coding
 {
     public static class Encoder
     {
+        /// <summary>
+        /// Encode vector of m+1 length using Reed Muller's code with parameter m.
+        /// Vector is multiplied with Generator matrix G(1,m)
+        /// </summary>
+        /// <param name="vector">Vector to encode</param>
+        /// <param name="m">Code parameter m</param>
+        /// <returns>Encoded vector</returns>
         public static Vector EncodeSingleVector (Vector vector, int m)
         {
             GeneratorMatrix.GeneratorMatrixes.TryGetValue(m, out int[,] generatorMatrix);
@@ -26,13 +33,25 @@ namespace Reed_Muller.Coding
                 resultArray[i] = resultArray[i] % 2;
             }
             return new Vector(resultArray);
-
         }
 
+
+        /// <summary>
+        /// Encode binary sequence using Reed Muller's code with parameter m.
+        /// Provided binary sequence is divided to vectors of valid length and each of them
+        /// is encoded separately. If the last vector's length after dividing the sequence 
+        /// is too short, additional 0 bits are added to it.
+        /// </summary>
+        /// <param name="sequence">Binary sequence - vector - to be encoded</param>
+        /// <param name="m">Code parameter m</param>
+        /// <param name="additionalBits">Amout of bits that were added to the last encoded vector</param>
+        /// <returns></returns>
         public static List<Vector> EncodeBinarySequence (Vector sequence, int m, out int additionalBits)
         {
             var result = new List<Vector>();
             var vectorLength = m + 1;
+
+            // Divide the sequence to a list of vector of correct length
             var vectors = sequence.Data
                 .Select((s, i) => sequence.Data.Skip(i * vectorLength).Take(vectorLength).ToArray())
                 .Where(a => a.Any()).Select(x => new Vector(x)).ToList();
@@ -42,6 +61,7 @@ namespace Reed_Muller.Coding
             var lastVectorLength = lastVectorData.Count();
 
             additionalBits = 0;
+            // Add additional bits to the last vector if neccessary
             if (lastVectorLength != vectorLength)
             {
                 vectors.Remove(lastVector);
@@ -51,6 +71,8 @@ namespace Reed_Muller.Coding
                 lastVector.Length = vectorLength;
                 vectors.Add(lastVector);
             }
+
+            // Encode each vector separately
             foreach(var v in vectors)
             {
                 var encoded = v.Encode(m);
